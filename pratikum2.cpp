5fde07aab6d07e1856c2b51cd09900c2f2e3dce5
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-03-10 15:51:48 +0700

    Update pratikum2.cpp

--- pratikum2.cpp
+++ pratikum2.cpp
@@ -17,7 +17,7 @@ float hitungBMI(float berat, float tinggi)
     return berat / (tinggi * tinggi);
 }
 
-// Fungsi menentukan
+// Fungsi 
 string kondisiBMI(float bmi)
 {
     if (bmi < 18.5)
@@ -46,3 +46,4 @@ int main()
     return 0;
 
 }
+
